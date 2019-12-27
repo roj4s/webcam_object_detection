02e29ceb7d3042034508a1c4e05c8f83f2753e8d
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2019-12-27 16:56:17 -0400

    Initial webcam detection commit to check on docker

---
+++ webcam_detection.py
@@ -0,0 +1,23 @@
+import cv2
+
+def show():
+    cap = cv2.VideoCapture(0)
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the resulting frame
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    cv2.VideoCapture(0)
+
+if  __name__ == "__main__":
+    show()
