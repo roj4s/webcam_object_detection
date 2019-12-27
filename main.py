525bffc99b2dae42eef09c6e657fe749a3a7dc55
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2019-12-27 15:55:00 -0400

    Detector

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+import nn.DarknetFrame as darknet
+import cv2
+
 class Detector:
 
-    def __init__(self):
-        pass
+    def __init__(self, config_file='nn/configs/yolov3-tiny.cfg',
+                 weights_file="nn/weights/yolov3-tiny.weights",
+                 classes_data_file="nn/configs/coco.data"):
 
-    def detect(self, frame):
-        pass
+        self.net_main, self.meta_main = darknet.load_nn(config_file,
+                                                        weights_file,
+                                                        classes_data_file)
+
+        # Create an image we reuse for each detect
+        self.darknet_image = darknet.make_image(
+            darknet.network_width(self.net_main),
+            darknet.network_height(self.net_main),
+            3)
+
+    def detect(self, frame, thresh=0.5):
+        frame_resized = cv2.resize(frame,
+                                   (darknet.network_width(self.net_main),
+                                    darknet.network_height(self.net_main)),
+                                   interpolation=cv2.INTER_LINEAR)
+
+        darknet.copy_image_from_bytes(
+            self.darknet_image, frame_resized.tobytes())
+
+        nn_detections = darknet.detect_image(
+            self.net_main, self.meta_main, self.darknet_image, thresh)
+
+        return nn_detections
