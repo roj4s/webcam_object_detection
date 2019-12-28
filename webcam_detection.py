f85e8626747a377e1b4e48282dae01332bde17f4
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2019-12-28 16:30:11 -0400

    Adding rectangle and label over detections

--- webcam_detection.py
+++ webcam_detection.py
@@ -1,17 +1,27 @@
 import cv2
+from detector import Detector
 
 def show():
     cap = cv2.VideoCapture(0)
+    detector = Detector()
 
     while(True):
         # Capture frame-by-frame
         ret, frame = cap.read()
+        detections = detector.detect(frame)
 
-        # Our operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        for d in detections:
+            p = (int(d.x), int(d.y))
+            top_left = (int(p[0] - d.w/2), int(p[1] - d.h/2))
+            bottom_right = (int(p[0] + d.w/2), int(p[1] + d.h/2))
+            frame = cv2.circle(frame, p, 3, (255, 0, 0), 3)
+            frame = cv2.rectangle(frame, top_left, bottom_right, (0, 255, 0), 1)
+            frame = cv2.putText(frame, d.label, p, cv2.FONT_HERSHEY_SIMPLEX, 1,
+                                (255, 0, 0), 2)
 
         # Display the resulting frame
-        cv2.imshow('frame',gray)
+        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
